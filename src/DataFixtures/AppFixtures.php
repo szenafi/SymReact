b9dcd04656411a78cf61aea640929f8c7bda8359
Said Zenafi <szenafi785@gmail.com>
2022-05-12 11:55:54 +0200

    Installation de api Platform

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,34 +3,54 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\User;
 use Faker\Generator;
 use App\Entity\Invoice;
 use App\Entity\Customer;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class AppFixtures extends Fixture
-{
+{ 
+   
     /**
      * @var Generator
      */
     private Generator $faker;
 
-    public function __construct()
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
     {
-        $this ->faker = Factory::create('fr_FR');  
-    }  
+        $this->faker = Factory::create();
+        $this->hasher = $hasher;
+    }
 
     public function load(ObjectManager $manager): void
-    {   $chrono = 1;
-        for($c = 0; $c < 10; $c++) {
+    {   
+        for($u = 0; $u < 10; $u++) {
+            $user = new User();
+            $chrono = 1;
+            $user ->setFirstName($this->faker->firstName)
+                    ->setLastName($this->faker->lastName)
+                    ->setEmail($this->faker->email)
+                    ->setPassword("password");
 
-            
+                $hashPassword = $this -> hasher -> hashPassword($user, "password");
+                $user -> setPassword($hashPassword);
+
+            $manager->persist($user);
+
+        for($c = 0; $c < mt_rand(5, 20); $c++) {
             $customer = new Customer();
             $customer -> setFirstName($this -> faker -> firstName)
                         -> setLastName($this -> faker -> lastName)
                         -> setEmail($this -> faker -> email)
-                        -> setCompany($this -> faker -> company);
+                        -> setCompany($this -> faker -> company)
+                        -> setUser($user);
+
             $manager -> persist($customer);
 
         for($i = 0; $i < 10; $i++) {
@@ -44,11 +64,13 @@ class AppFixtures extends Fixture
             $manager -> persist($invoice);
             $chrono++;
         }
+        }            
         }
+        
         
         // $product = new Product();
         // $manager->persist($product);
 
         $manager->flush();
     }
-    }
+}
